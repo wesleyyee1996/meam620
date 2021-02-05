f7ce25dbd5a4cdb754a38efd0bd8c697b181feb2
wesleyyee1996 <wesleyyee1996@gmail.com>
2021-02-04 19:19:02 -0600

    finish hw1 code

---
+++ hw1/hw1_q4.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+t = np.linspace(-10 * np.pi, 10 * np.pi, 100)
+
+px = np.cos(0.1*t)
+py = pz = np.sin(0.1*t)
+
+c = 0.25
+x = c * np.cos(t) + np.cos(0.1*t)
+y = c * np.sin(t) + np.sin(0.1*t)
+z = np.sin(0.1*t)
+ax.plot(x, y, z, label='Ap')
+ax.plot(px, py, pz, label='position')
+ax.legend()
+
+plt.show()
