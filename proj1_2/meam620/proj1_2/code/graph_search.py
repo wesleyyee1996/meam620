b8e675c147d9ae1d1c34cac91abb5415515e58ec
wesleyyee1996 <wesleyyee1996@gmail.com>
2021-02-24 20:23:14 -0600

    fixed some bugs

--- proj1_2/meam620/proj1_2/code/graph_search.py
+++ proj1_2/meam620/proj1_2/code/graph_search.py
@@ -1,6 +1,5 @@
 from heapq import heappush, heappop  # Recommended.
 import numpy as np
-from node import Node
 import heapq
 
 from flightsim.world import World
@@ -70,14 +69,18 @@ def graph_search(world, resolution, margin, start, goal, astar):
     while curr_node != start_node:                                      # create path now, starting from goal and
         if (curr_node.parent is None):                                  # retrace steps until start
             break
-        path.append(occ_map.index_to_metric_center(curr_node.index))
+        path.insert(0,occ_map.index_to_metric_center(curr_node.index))
         curr_node = curr_node.parent
 
+    # append start and end to path
+    path.insert(0, start)
+    path.append(goal)
+
     path = np.array(path)
-    if len(path)==1:
+    if len(path)==0:
         path = None
 
-    
+
 
     # Return a tuple (path, nodes_expanded)
     return (path, len(closed))
@@ -107,3 +110,21 @@ def get_neighbors(node, map):
                             list_neighbors[(i,j,k)] = cost+node.g
     return list_neighbors
 
+class Node:
+
+    def __init__(self, g, h, index, parent):
+        self.f = g+h                # total cost
+        self.g = g                  # cost to come
+        self.h = h                  # heuristic
+        self.index = index          # (i,j,k)
+        self.parent = parent        # (i,j,k)
+        self.is_closed = False      # True if node has been closed
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+    def __repr__(self):
+        if self.parent:
+            return f"Node g={self.g}, h={self.h}, index={self.index}, parent={self.parent.index}, is_closed={self.is_closed}"
+        else:
+            return f"Node g={self.g}, h={self.h}, index={self.index}, parent=None, is_closed={self.is_closed}"
